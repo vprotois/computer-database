860f49900e612ad46e7f5319a6b772b7bd87f830
vprotois <vprotois@excilys.com>
2019-03-20 15:52:08 +0100

    ajout DTOComputer

---
+++ computerdatabase/src/main/java/mapper/DTOComputerMapper.java
@@ -0,0 +1,29 @@
+package mapper;
+
+import model.Computer;
+import model.DTOComputer;
+
+public class DTOComputerMapper {
+
+	
+	
+	public static DTOComputer mapComputerToDTO(Computer c) {
+		DTOComputer dto = new DTOComputer();
+		dto.id = c.getId().longValue();
+		dto.name = c.getName();
+		if(c.getIntroduced() == null) {
+			dto.introduced = "";			
+		}else {
+			dto.introduced = c.getIntroduced().toString();
+		}
+		if(c.getDiscontinued() == null) {
+			dto.discontinued = "";			
+		}else {
+			dto.discontinued = c.getDiscontinued().toString();
+		}
+		dto.companyId = c.getCompanyId().longValue();
+		
+		return dto;
+	}
+	
+}
---
+++ computerdatabase/src/main/java/model/DTOComputer.java
@@ -0,0 +1,11 @@
+package model;
+
+
+public class DTOComputer {
+
+	public long id;
+	public String name;
+	public String introduced, discontinued;
+	public long companyId;
+	
+}
