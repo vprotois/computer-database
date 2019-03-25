30f87977e54d2de4625c324bf6a2ba544c8ee6fc
vprotois <vprotois@excilys.com>
2019-03-25 16:19:36 +0100

    fixed mapper test

--- computerdatabase/src/test/java/mapper/CompanyMapperTest.java
+++ computerdatabase/src/test/java/mapper/CompanyMapperTest.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -47,7 +48,9 @@ class CompanyMapperTest {
 	
 	@Test
 	void testSingleCompany() throws SQLException {
-		Company c = CompanyMapper.mapSingleCompany(rs);
+		Optional<Company> optc = CompanyMapper.mapSingleCompany(rs);
+		assertTrue(optc.isPresent());
+		Company c = optc.get();
 		assertEquals(new Long(1),c.getId());
 		assertEquals("azaz1234",c.getName());
 	}
