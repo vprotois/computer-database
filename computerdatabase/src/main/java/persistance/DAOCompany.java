50c769371d4a7e86de88b5e01a49e7c7ab79f200
vprotois <vprotois@excilys.com>
2019-03-29 12:18:42 +0100

    added Company delete in DAO

--- computerdatabase/src/main/java/persistance/DAOCompany.java
+++ computerdatabase/src/main/java/persistance/DAOCompany.java
@@ -14,22 +14,24 @@ import mapper.CompanyMapper;
 import model.Company;
 
 public class DAOCompany {
-		
+
 	private static Logger log= LoggerFactory.getLogger(DAOCompany.class);
-	
+
 	private static final String selectAll ="SELECT id,name FROM company;";
 	private static final String selectId = "SELECT id,name FROM company WHERE id = ?;";
-	
+	private static final String deleteFromId = "DELETE FROM company WHERE id = ?;";
+	private static final String deleteComputers = "DELETE FROM computer WHERE company_id = ?;";
+
 	public DAOCompany() {
 		super();
 	}
-	
+
 	public Optional<Company> getCompany(Long id){
 		try (Connection conn = ConnectionPool.getDataSource().getConnection()) {
 			PreparedStatement stmt = conn.prepareStatement(selectId);
 			stmt.setLong(1,id);
 			ResultSet results =  stmt.executeQuery();
-			
+
 			return CompanyMapper.mapSingleCompany(results);
 		}catch(SQLException e) {
 			System.out.println(e.getMessage());
@@ -44,11 +46,43 @@ public class DAOCompany {
 			ResultSet results =  stmt.executeQuery();
 			return Optional.of(CompanyMapper.mapCompaniesList(results));
 		}catch(SQLException e){
-			System.out.println(e.getMessage());
-			log.error("Error when getting company list");
+			log.error("Error when getting company list :"+e.getMessage());
 		}
 		return Optional.empty();
 	}
 
-	
+
+	public void deleteCompany(Long id) {
+		Connection conn = null;
+		try {
+			conn = ConnectionPool.getDataSource().getConnection();
+
+			try  {
+				conn.setAutoCommit(false);
+				PreparedStatement stmt = conn.prepareStatement(deleteComputers);
+				stmt.setLong(1, id);
+				stmt.executeUpdate();
+				stmt = conn.prepareStatement(deleteFromId);
+				stmt.setLong(1, id);
+				stmt.executeUpdate();
+				conn.commit();
+			} catch (SQLException e) {
+				if(conn != null) {
+					conn.rollback();
+
+				}
+				log.error("Error when deleting company :"+e.getMessage());
+			}finally {
+
+				if (conn != null && ! conn.isClosed()) {
+					conn.close();
+
+
+				}
+			} 
+		}catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+	}
+
 }
